c1e035dca3b1c6c8e6952507c7d8cf296c349fab
Devmithra Panwar Arya <dev87111@gmail.com>
2025-09-10 22:04:14 +0530

    added delete in products.java

--- src/main/java/com/example/demo/Controllers/ProductsController.java
+++ src/main/java/com/example/demo/Controllers/ProductsController.java
@@ -3,8 +3,10 @@ package com.example.demo.Controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +41,11 @@ public class ProductsController {
     public String updateProduct(@RequestBody products product) {
         productsRepository.save(product);
         return "Product Updated";
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable String id) {
+        productsRepository.deleteById(id);
+        return "Product Deleted";
     }
 }
--- src/main/java/com/example/demo/models/products.java
+++ src/main/java/com/example/demo/models/products.java
@@ -16,6 +16,10 @@ public class products {
     public String getId() {
         return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
     public String getName() {
         return name;
